939ae2cf2ada328eb680b2333a575ebee5d2b79e
xlwzzzy <marioaistwice@gmail.com>
2023-12-28 09:54:09 +0700

    resolve yarn err

--- src/registry/default/example/calendar-form.tsx
+++ src/registry/default/example/calendar-form.tsx
@@ -1,14 +1,14 @@
-"use client"
+'use client';
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { format } from "date-fns"
-import { CalendarIcon } from "lucide-react"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
+import { zodResolver } from '@hookform/resolvers/zod';
+import { format } from 'date-fns';
+import { CalendarIcon } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/registry/default/ui/button"
-import { Calendar } from "@/registry/default/ui/calendar"
+import { cn } from '@/lib/utils';
+import { Button } from '@/registry/default/ui/button';
+import { Calendar } from '@/registry/default/ui/calendar';
 import {
   Form,
   FormControl,
@@ -17,34 +17,30 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/registry/default/ui/form"
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/registry/default/ui/popover"
-import { toast } from "@/registry/default/ui/use-toast"
+} from '@/registry/default/ui/form';
+import { Popover, PopoverContent, PopoverTrigger } from '@/registry/default/ui/popover';
+import { toast } from '@/registry/default/ui/use-toast';
 
 const FormSchema = z.object({
   dob: z.date({
-    required_error: "A date of birth is required.",
+    required_error: 'A date of birth is required.',
   }),
-})
+});
 
 export default function CalendarForm() {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
-  })
+  });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
     toast({
-      title: "You submitted the following values:",
+      title: 'You submitted the following values:',
       description: (
         <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
           <code className="text-white">{JSON.stringify(data, null, 2)}</code>
         </pre>
       ),
-    })
+    });
   }
 
   return (
@@ -53,24 +49,17 @@ export default function CalendarForm() {
         <FormField
           control={form.control}
           name="dob"
-          render={({ field }) => (
+          render={({ field }: any) => (
             <FormItem className="flex flex-col">
               <FormLabel>Date of birth</FormLabel>
               <Popover>
                 <PopoverTrigger asChild>
                   <FormControl>
                     <Button
-                      variant={"outline"}
-                      className={cn(
-                        "w-[240px] pl-3 text-left font-normal",
-                        !field.value && "text-muted-foreground"
-                      )}
+                      variant={'outline'}
+                      className={cn('w-[240px] pl-3 text-left font-normal', !field.value && 'text-muted-foreground')}
                     >
-                      {field.value ? (
-                        format(field.value, "PPP")
-                      ) : (
-                        <span>Pick a date</span>
-                      )}
+                      {field.value ? format(field.value, 'PPP') : <span>Pick a date</span>}
                       <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
                     </Button>
                   </FormControl>
@@ -80,16 +69,12 @@ export default function CalendarForm() {
                     mode="single"
                     selected={field.value}
                     onSelect={field.onChange}
-                    disabled={(date) =>
-                      date > new Date() || date < new Date("1900-01-01")
-                    }
+                    disabled={(date) => date > new Date() || date < new Date('1900-01-01')}
                     initialFocus
                   />
                 </PopoverContent>
               </Popover>
-              <FormDescription>
-                Your date of birth is used to calculate your age.
-              </FormDescription>
+              <FormDescription>Your date of birth is used to calculate your age.</FormDescription>
               <FormMessage />
             </FormItem>
           )}
@@ -97,5 +82,5 @@ export default function CalendarForm() {
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
+  );
 }
--- src/registry/default/example/checkbox-form-multiple.tsx
+++ src/registry/default/example/checkbox-form-multiple.tsx
@@ -1,11 +1,11 @@
-"use client"
+'use client';
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
 
-import { Button } from "@/registry/default/ui/button"
-import { Checkbox } from "@/registry/default/ui/checkbox"
+import { Button } from '@/registry/default/ui/button';
+import { Checkbox } from '@/registry/default/ui/checkbox';
 import {
   Form,
   FormControl,
@@ -14,59 +14,59 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/registry/default/ui/form"
-import { toast } from "@/registry/default/ui/use-toast"
+} from '@/registry/default/ui/form';
+import { toast } from '@/registry/default/ui/use-toast';
 
 const items = [
   {
-    id: "recents",
-    label: "Recents",
+    id: 'recents',
+    label: 'Recents',
   },
   {
-    id: "home",
-    label: "Home",
+    id: 'home',
+    label: 'Home',
   },
   {
-    id: "applications",
-    label: "Applications",
+    id: 'applications',
+    label: 'Applications',
   },
   {
-    id: "desktop",
-    label: "Desktop",
+    id: 'desktop',
+    label: 'Desktop',
   },
   {
-    id: "downloads",
-    label: "Downloads",
+    id: 'downloads',
+    label: 'Downloads',
   },
   {
-    id: "documents",
-    label: "Documents",
+    id: 'documents',
+    label: 'Documents',
   },
-] as const
+] as const;
 
 const FormSchema = z.object({
   items: z.array(z.string()).refine((value) => value.some((item) => item), {
-    message: "You have to select at least one item.",
+    message: 'You have to select at least one item.',
   }),
-})
+});
 
 export default function CheckboxReactHookFormMultiple() {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      items: ["recents", "home"],
+      items: ['recents', 'home'],
     },
-  })
+  });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
     toast({
-      title: "You submitted the following values:",
+      title: 'You submitted the following values:',
       description: (
         <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
           <code className="text-white">{JSON.stringify(data, null, 2)}</code>
         </pre>
       ),
-    })
+    });
   }
 
   return (
@@ -79,40 +79,29 @@ export default function CheckboxReactHookFormMultiple() {
             <FormItem>
               <div className="mb-4">
                 <FormLabel className="text-base">Sidebar</FormLabel>
-                <FormDescription>
-                  Select the items you want to display in the sidebar.
-                </FormDescription>
+                <FormDescription>Select the items you want to display in the sidebar.</FormDescription>
               </div>
               {items.map((item) => (
                 <FormField
                   key={item.id}
                   control={form.control}
                   name="items"
-                  render={({ field }) => {
+                  render={({ field }: any) => {
                     return (
-                      <FormItem
-                        key={item.id}
-                        className="flex flex-row items-start space-x-3 space-y-0"
-                      >
+                      <FormItem key={item.id} className="flex flex-row items-start space-x-3 space-y-0">
                         <FormControl>
                           <Checkbox
                             checked={field.value?.includes(item.id)}
                             onCheckedChange={(checked) => {
                               return checked
                                 ? field.onChange([...field.value, item.id])
-                                : field.onChange(
-                                    field.value?.filter(
-                                      (value) => value !== item.id
-                                    )
-                                  )
+                                : field.onChange(field.value?.filter((value: any) => value !== item.id));
                             }}
                           />
                         </FormControl>
-                        <FormLabel className="font-normal">
-                          {item.label}
-                        </FormLabel>
+                        <FormLabel className="font-normal">{item.label}</FormLabel>
                       </FormItem>
-                    )
+                    );
                   }}
                 />
               ))}
@@ -123,5 +112,5 @@ export default function CheckboxReactHookFormMultiple() {
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
+  );
 }
